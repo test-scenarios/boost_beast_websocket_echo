9baf3c2ad6385fb6f7d852257cac0ebdfbc5263c
Richard Hodges <hodges.r@gmail.com>
2020-10-29 12:00:04 +0100

    mime_reader works on both chunked and non-chunked

--- pre-cxx20/mime_reader/config.hpp
+++ pre-cxx20/mime_reader/config.hpp
@@ -1,17 +1,15 @@
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
-
-#include <boost/asio.hpp>
 #include <fmt/ostream.h>
 #include <regex>
 #include <spdlog/spdlog.h>
 
-namespace program
-{
-    namespace beast = boost::beast;
-    namespace http  = beast::http;
-    namespace asio  = boost::asio;
-    namespace net   = asio;
+namespace program {
+namespace beast = boost::beast;
+namespace http  = beast::http;
+namespace asio  = boost::asio;
+namespace net   = asio;
 
-    using boost::system::error_code;
+using boost::system::error_code;
+using boost::system::system_error;
 }   // namespace program
--- pre-cxx20/mime_reader/main.cpp
+++ pre-cxx20/mime_reader/main.cpp
@@ -1,6 +1,4 @@
 #include "config.hpp"
-#include "http_stream.hpp"
-#include "mime_reader.hpp"
 
 #include <boost/beast/_experimental/test/stream.hpp>
 #include <iostream>
@@ -73,31 +71,212 @@ chunkify(int chunks, std::string input)
     return oss.str();
 }
 
+error_code
+test(beast::string_view sample)
+{
+    try
+    {
+        net::io_context ioc;
+        auto            exec = ioc.get_executor();
+
+        auto spin = [&ioc](std::string const &message) {
+            spdlog::debug("spin: {}", message);
+            ioc.restart();
+            std::size_t count = 0;
+            while (auto c = ioc.poll())
+                count += c;
+            return count;
+        };
+
+        auto local =
+            beast::test::stream(ioc);   // simulate the local socket/tls stream
+        auto remote = connect(
+            local);   // simulate the remote socket so we can push in test data
+
+        remote.write_some(net::buffer(sample.data(), sample.size()));
+
+        error_code  ec;
+        std::size_t bytes_transferred = 0;
+        auto handler = [&](error_code ec_, std::size_t bytes_transferred_) {
+            ec                = ec_;
+            bytes_transferred = bytes_transferred_;
+        };
+        auto quit_check = [&] {
+            if (ec)
+            {
+                spdlog::error("read error {}", ec);
+                throw system_error(ec);
+            }
+        };
+
+        http::response_parser< http::buffer_body > parser;
+        beast::multi_buffer                        rxbuf;
+
+        //
+        // This is where you read the header
+        //
+
+        parser.eager(false);
+        std::string work_store;
+        auto        outbuf = asio::dynamic_buffer(work_store);
+
+        {
+            outbuf.grow(256);
+            auto prev                = outbuf.size();
+            auto dat                 = outbuf.data(prev, 256);
+            parser.get().body().data = dat.data();
+            parser.get().body().size = dat.size();
+            parser.get().body().more = true;
+            http::async_read_header(local, rxbuf, parser, handler);
+            spin("reading header");
+            outbuf.shrink(256 - bytes_transferred);
+            quit_check();
+        }
+
+        auto boundary =
+            deduce_boundary(parser.get()[http::field::content_type]);
+        if (not boundary)
+        {
+            spdlog::error("Not mime");
+            return error_code(boost::system::errc::protocol_error,
+                              boost::system::generic_category());
+        }
+
+        spdlog::info("detected mime boundary: {}, chunked: {}",
+                     *boundary,
+                     parser.chunked());
+
+        //
+        // now we want to scan the input looking for the boundary
+        //
+
+        bool first_part          = true;
+        bool expect_chunk_header = true;
+
+        auto on_chunk_body =
+            [&](std::uint64_t remain, beast::string_view body, error_code &ec) {
+                auto pos = outbuf.size();
+                auto len = body.size();
+                outbuf.grow(len);
+                net::buffer_copy(outbuf.data(pos, len),
+                                 net::buffer(body.data(), len));
+                expect_chunk_header = (remain == 0);
+                return len;
+            };
+        parser.on_chunk_body(on_chunk_body);
+
+        auto        boundary_pos = std::string::size_type(0);
+        std::string boundary_line;
+
+    next_boundary:
+        for(;;)
+        {
+            boundary_line = "--" + *boundary + "\r\n";
+            boundary_pos  = work_store.find(boundary_line);
+            if (boundary_pos >= work_store.size())
+            {
+                boundary_line = "--" + *boundary + "--\r\n";
+                boundary_pos  = work_store.find(boundary_line);
+            }
+
+            // found boundary
+            if(boundary_pos < work_store.size())
+                break;
+
+            if (parser.chunked())
+            {
+                while (expect_chunk_header)
+                {
+                    http::async_read_some(local, rxbuf, parser, handler);
+                    spin("skipping chunk header");
+                }
+                http::async_read_some(local, rxbuf, parser, handler);
+                spin("seeking boundary");
+            }
+            else
+            {
+                outbuf.grow(512);
+                auto dat = outbuf.data(outbuf.size() - 512, outbuf.size());
+                parser.get().body().data = dat.data();
+                parser.get().body().size = dat.size();
+                parser.get().body().more = false;
+                http::async_read_some(local, rxbuf, parser, handler);
+                spin("seeking boundary");
+                outbuf.shrink(512 - parser.get().body().size);
+            }
+            quit_check();
+        }
+
+        if (!first_part)
+        {
+            // process the previous part
+            std::cout << "***HERE IS A PART: "
+                      << work_store.substr(0, boundary_pos - 2) << std::endl;
+        }
+
+        if (boundary_line == "--" + *boundary + "--\r\n")
+        {
+            spdlog::info("End of Stream");
+            return error_code();
+        }
+
+        work_store.erase(0, boundary_pos + boundary_line.size());
+
+        // now pick up headers
+    next_header:
+        auto hdr_pos = work_store.find("\r\n");
+        if (hdr_pos == std::string::npos)
+        {
+            if (parser.chunked())
+            {
+                while (expect_chunk_header)
+                {
+                    http::async_read_some(local, rxbuf, parser, handler);
+                    spin("skipping chunk header");
+                }
+                http::async_read_some(local, rxbuf, parser, handler);
+                spin("seeking boundary header");
+            }
+            else
+            {
+                outbuf.grow(512);
+                auto dat = outbuf.data(outbuf.size() - 512, outbuf.size());
+                parser.get().body().data = dat.data();
+                parser.get().body().size = dat.size();
+                parser.get().body().more = false;
+                http::async_read_some(local, rxbuf, parser, handler);
+                spin("seeking boundary header");
+                outbuf.shrink(512 - parser.get().body().size);
+            }
+            quit_check();
+            goto next_header;
+        }
+        if (hdr_pos == 0)
+        {
+            // end of headers
+        }
+        else
+        {
+            spdlog::info("part header: {}", work_store.substr(0, hdr_pos));
+            work_store.erase(0, hdr_pos + 2);
+            goto next_header;
+        }
+
+        first_part = false;
+        goto next_boundary;
+    }
+    catch (system_error &ec)
+    {
+        return ec.code();
+    }
+}
+
 }   // namespace program
 
 int
 main()
 {
     using namespace program;
-
-    spdlog::set_level(spdlog::level::info);
-
-    net::io_context ioc;
-    auto            exec = ioc.get_executor();
-
-    auto spin = [&ioc](std::string const &message) {
-        std::cout << "debug: " << message << std::endl;
-        ioc.restart();
-        std::size_t count = 0;
-        while (auto c = ioc.poll())
-            count += c;
-        return count;
-    };
-
-    auto local =
-        beast::test::stream(ioc);   // simulate the local socket/tls stream
-    auto remote = connect(
-        local);   // simulate the remote socket so we can push in test data
 
     // now simulate our host's read
 
@@ -124,158 +303,13 @@ main()
             "\r\n") +
         payload2;
 
-    bool do_chunked = true;
-    remote.write_some(do_chunked ? net::buffer(chunked)
-                                 : net::buffer(unchunked));
+    std::cout << "Test CHUNKED\n"
+                 "============\n\n";
+    test(chunked);
 
-    error_code  ec;
-    std::size_t bytes_transferred = 0;
-    auto        handler = [&](error_code ec_, std::size_t bytes_transferred_) {
-        ec                = ec_;
-        bytes_transferred = bytes_transferred_;
-    };
-    auto quit_check = [&] {
-        if (ec)
-        {
-            std::cerr << "error: " << ec.message() << std::endl;
-            std::exit(1);
-        }
-    };
+    std::cout << "\n"
+                 "Test NOT CHUNKED\n"
+                 "================\n\n";
 
-    http::response_parser< http::buffer_body > parser;
-    beast::multi_buffer                        rxbuf;
-
-    //
-    // This is where you read the header
-    //
-
-    parser.eager(false);
-    http::async_read_header(local, rxbuf, parser, handler);
-    spin("reading header");
-    quit_check();
-
-    auto boundary = deduce_boundary(parser.get()[http::field::content_type]);
-    if (not boundary)
-    {
-        std::cerr << "Not mime\n";
-    }
-
-    std::cout << "detected mime boundary: " << *boundary << std::endl;
-    std::cout << "chunked: " << (parser.chunked() ? "yes" : "no") << std::endl;
-
-    //
-    // now we want to scan the input looking for the boundary
-    //
-
-    std::string work_store;
-    bool        first_part          = true;
-    bool        expect_chunk_header = true;
-    auto        outbuf              = asio::dynamic_buffer(work_store);
-
-    auto on_chunk_body = [&](std::uint64_t      remain,
-                             beast::string_view body,
-                             error_code &       ec) {
-        auto pos = outbuf.size();
-        auto len = body.size();
-        outbuf.grow(len);
-        net::buffer_copy(outbuf.data(pos, len), net::buffer(body.data(), len));
-        expect_chunk_header = (remain == 0);
-        return len;
-    };
-    parser.on_chunk_body(on_chunk_body);
-
-    auto        boundary_pos = std::string::size_type(0);
-    std::string boundary_line;
-
-next_boundary:
-    do
-    {
-        if (parser.chunked())
-        {
-            while (expect_chunk_header)
-            {
-                http::async_read_some(local, rxbuf, parser, handler);
-                spin("skipping chunk header");
-            }
-            http::async_read_some(local, rxbuf, parser, handler);
-            spin("seeking boundary");
-        }
-        else
-        {
-            outbuf.grow(512);
-            auto dat = outbuf.data(outbuf.size() - 512, outbuf.size());
-            parser.get().body().data = dat.data();
-            parser.get().body().size = dat.size();
-            parser.get().body().more = false;
-            http::async_read_some(local, rxbuf, parser, handler);
-            spin("seeking boundary");
-            outbuf.shrink(512 - bytes_transferred);
-        }
-        quit_check();
-        boundary_line = "--" + *boundary + "\r\n";
-        boundary_pos  = work_store.find(boundary_line);
-        if (boundary_pos >= work_store.size())
-        {
-            boundary_line = "--" + *boundary + "--\r\n";
-            boundary_pos  = work_store.find(boundary_line);
-        }
-    } while (boundary_pos >= work_store.size());
-
-    if (!first_part)
-    {
-        // process the previous part
-        std::cout << "***HERE IS A PART: "
-                  << work_store.substr(0, boundary_pos - 2) << std::endl;
-    }
-
-    if (boundary_line == "--" + *boundary + "--\r\n")
-    {
-        std::cout << "End of Stream\n";
-        std::exit(0);
-    }
-
-    work_store.erase(0, boundary_pos + boundary_line.size());
-
-// now pick up headers
-next_header:
-    auto hdr_pos = work_store.find("\r\n");
-    if (hdr_pos == std::string::npos)
-    {
-        if (parser.chunked())
-        {
-            while (expect_chunk_header)
-            {
-                http::async_read_some(local, rxbuf, parser, handler);
-                spin("skipping chunk header");
-            }
-            http::async_read_some(local, rxbuf, parser, handler);
-            spin("seeking boundary header");
-        }
-        else
-        {
-            outbuf.grow(512);
-            auto dat = outbuf.data(outbuf.size() - 512, outbuf.size());
-            parser.get().body().data = dat.data();
-            parser.get().body().size = dat.size();
-            parser.get().body().more = false;
-            http::async_read_some(local, rxbuf, parser, handler);
-            spin("seeking boundary header");
-            outbuf.shrink(512 - parser.get().body().size);
-        }
-        quit_check();
-        goto next_header;
-    }
-    if (hdr_pos == 0)
-    {
-        // end of headers
-    }
-    else
-    {
-        std::cout << "-- " << work_store.substr(0, hdr_pos) << std::endl;
-        work_store.erase(0, hdr_pos + 2);
-        goto next_header;
-    }
-
-    first_part = false;
-    goto next_boundary;
+    test(unchunked);
 }
